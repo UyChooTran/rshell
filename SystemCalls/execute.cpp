71aff28782c0b01f43c3e5e2040dfa4414eecba3
UyTran <uytran9321@gmail.com>
2018-02-18 12:34:50 -0800

    System call example

---
+++ SystemCalls/execute.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main()
+{
+    char* buf[3];
+    buf[0] = "ls";
+    buf[1] = "-la";
+    buf[2] = NULL;
+    char* command = "ls";
+
+      pid_t pid;
+
+      int status;
+
+     if ((pid = fork()) < 0) {     
+          printf("*** ERROR: forking child process failed\n");
+          exit(1);
+     }
+     else if (pid == 0) {          
+          if (execvp(command, buf) < 0) {     
+               printf("*** ERROR: exec failed\n");
+               exit(1);
+          }
+     }
+     else                                  
+          while (wait(&status) != pid );
+
+
+
+         printf(">");
+
+
+
+
+
+
+    return 0;
+}
