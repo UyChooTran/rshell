07460f3ca61237687dbbdb5681e60ed526395430
UyTran <uytran9321@gmail.com>
2018-02-18 21:40:15 -0800

    Extra functions

--- src/RShell.cpp
+++ src/RShell.cpp
@@ -7,12 +7,15 @@ using namespace std;
 
 RShell::RShell(){
   this->exit = false;
+  this->strings = new queue<string>();
+  this->inputs = queue<Input*>();
   this->input();
 }
 
 void RShell::input(){
   output();
   getline(cin, cmd);
+  this->parseCommands();
 }
 
 void RShell::output(){
@@ -29,15 +32,48 @@ void RShell::output(string str){
   return;
 }
 
-void RShell::parseCommands(queue<Input*>* fill){
+void RShell::parseCommands(){
   stringstream parse(cmd);
   string inString;
-
   while(parse.good()){
     parse >> inString;
-    fill->push(new Input(inString));
+    strings->push(inString);
   }
 
+}
+
+void RShell::createInputs(){
+  
+  vector<string>* commands = new vector<string>();
+  string parse;
+
+  while(!this->strings.empty()){
+    parse = strings.front();
+    strings.pop();
+    
+    if(parse == "||"){
+      inputs.push(new Or();
+    }
+    else if(parse == "&&"){
+      inputs.push(new And();
+    }
+    else if(parse.front() == '#'){
+      inputs.push(new Comment();
+    }
+    else if(parse.back() != ';'){
+      commands->push_back(parse);
+    }
+    else{
+      parse.pop_back();
+      commands->push_back(parse);
+      inputs.push(new Command(commands) 
+      delete commands;
+      commands = new vector<string>();
+    }
+
+  }
+  
+  delete commands;
 }
 
 bool RShell::getExitStatus(){
